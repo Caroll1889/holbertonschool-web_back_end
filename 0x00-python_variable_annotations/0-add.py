9f7fb69afa89285547c3970514291773cc6d5a93
Caroll1889 <diahancaroll@hotmail.com>
2021-03-15 11:56:38 -0500

    Fix: pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,5 +3,8 @@
 """Basic annotations - add
 mandatory"""
 
-def add(a:float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
+    """function that takes a float a and a float b
+    as arguments and returns their sum as a float."""
     return (a + b)
