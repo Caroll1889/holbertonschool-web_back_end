68c150f307df61e8e2a1aa9650333ffc23f44b28
Caroll1889 <diahancaroll@hotmail.com>
2021-03-21 07:40:30 -0500

    Lifo Algorithm

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" FIFO caching
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Class LifoCache"""
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.line = []
+
+    def put(self, key, item):
+        """ Add an item in the cache with LIFO algorithm """
+        
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            erase = self.line.pop()
+            del self.cache_data[erase]
+            print('DISCARD: {}'.format(erase))
+        
+        if key not in self.line:
+            self.line.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
