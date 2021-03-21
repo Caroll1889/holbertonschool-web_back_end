015883781bb7ba2a3b747475ce26bb45c8ee7086
Caroll1889 <diahancaroll@hotmail.com>
2021-03-21 07:31:24 -0500

    Fix: Docstring

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -6,14 +6,14 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """Basic Cache"""
     def put(self, key, item):
         """ Add an item in the cache """
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
         if key is None or key not in self.cache_data:
             return None
-        else:
-            return self.cache_data[key]
+        return self.cache_data[key]
