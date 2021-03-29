977bdcfb3138928fa9df4709886b8ed0dabc4ef4
Caroll1889 <diahancaroll@hotmail.com>
2021-03-29 16:20:18 -0500

    Add: required authentication

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -11,8 +11,21 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Required authentication"""
-        return False
-
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+        if path[-1] != '/': # Comprobar si el path no termina en '/'
+            path += '/'
+        for i in excluded_paths:
+            if i == path:
+                return False
+            if i[-1] != '/':
+                i += '/'
+            return True
+        
     def authorization_header(self, request=None) -> str:
         """Authentication header"""
         return None
