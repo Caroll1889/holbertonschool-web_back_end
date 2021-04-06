ae10bcc88fadde01ca40eff459b14713e9d023e2
Caroll1889 <diahancaroll@hotmail.com>
2021-04-06 08:36:50 -0500

    Fix: user_object_from_credentials method

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,9 +54,9 @@ class BasicAuth(Auth):
     def user_object_from_credentials(self, user_email:
                                      str, user_pwd: str) -> TypeVar('User'):
         """Basic - User object"""
-        if user_email is None or not isinstance(user_email, str):
+        if not user_email or not isinstance(user_email, str):
             return None
-        if user_pwd is None or not isinstance(user_pwd, str):
+        if not user_pwd or not isinstance(user_pwd, str):
             return None
         user = User.search({'email': user_email})
         # if not user:
