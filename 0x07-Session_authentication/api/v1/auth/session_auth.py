f18f6e1940b7e34509d2d9671506b2625b07037e
Caroll1889 <diahancaroll@hotmail.com>
2021-04-06 13:40:09 -0500

    Fix: Docstring

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -10,6 +10,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """Function that creates a Session ID for a user_id"""
         if user_id is None or not isinstance(user_id, str):
             return None
         session = str(uuid4())
@@ -18,6 +19,7 @@ class SessionAuth(Auth):
         return session
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """function that returns a User ID based on a Session ID:"""
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
