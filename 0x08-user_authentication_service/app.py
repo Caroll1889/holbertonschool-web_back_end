88421665b69fb7fab7209c583c47a791afc594cc
Caroll1889 <diahancaroll@hotmail.com>
2021-04-11 18:17:13 -0500

    end-point to register a user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,10 @@
 """Set up a basic Flask app.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -12,6 +15,19 @@ app = Flask(__name__)
 def index() -> str:
     """Basic Flask app"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """End-point to register a user."""
+    email = request.form.get('email')
+    passwd = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, passwd)
+        return jsonify({f"email": email, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
