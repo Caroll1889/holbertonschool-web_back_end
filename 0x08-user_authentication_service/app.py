cc3bcfd0f5d5e851c7f79d401787aa6ddeb01a42
Caroll1889 <diahancaroll@hotmail.com>
2021-04-12 16:30:59 -0500

    Login method

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Set up a basic Flask app.
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -28,6 +28,21 @@ def users():
         return jsonify({f"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Log in"""
+
+    email = request.form.get('email')
+    passwd = request.form.get('password')
+
+    if not AUTH.valid_login(email, passwd):
+        abort(401)
+    new_id = AUTH.create_session(email)
+    res = jsonify({"email": email, "message": "logged in"})
+    res.set_cookie('session_id', new_id)
+    return res
 
 
 if __name__ == "__main__":
