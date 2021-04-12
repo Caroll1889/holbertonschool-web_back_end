515e3eb0d17cc99a85d21d16fc033ee2a0dd5225
Caroll1889 <diahancaroll@hotmail.com>
2021-04-12 16:39:27 -0500

    Find a user from session id

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -59,3 +59,14 @@ class Auth:
             return new_uuid
         except Exception:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Find user by session ID"""
+        if not session_id:
+            return None
+
+        try:
+            user = user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
