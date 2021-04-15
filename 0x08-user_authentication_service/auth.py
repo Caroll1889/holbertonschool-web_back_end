e114319cc60cba3e69a947fbab0daa274ea1a9d2
Caroll1889 <diahancaroll@hotmail.com>
2021-04-15 12:22:59 -0500

    Add: Update password method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -90,7 +90,7 @@ class Auth:
         """Update password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            passwd = self._db._hash_password(password)
+            passwd = _hash_password(password)
             self._db.update_user(user.id, hashed_password=passwd,
                                  reset_token=None)
             return None
