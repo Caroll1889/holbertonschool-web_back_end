f7fc7ea6d815a55e5ace1a4e7d82fac2d51ef5b2
Caroll1889 <diahancaroll@hotmail.com>
2021-04-15 12:21:33 -0500

    Add: Update password methor

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -85,3 +85,14 @@ class Auth:
             return uui
         except Exception:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            passwd = self._db._hash_password(password)
+            self._db.update_user(user.id, hashed_password=passwd,
+                                 reset_token=None)
+            return None
+        except Exception:
+            raise ValueError
