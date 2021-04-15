9401d3f6c52275dde6cd8d3e1b290a6864864c85
Caroll1889 <diahancaroll@hotmail.com>
2021-04-14 21:52:49 -0500

    Add: reset_password route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -75,12 +75,11 @@ def profile() -> str:
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
     """Function that gets reset password token"""
-
     email = request.form.get('email')
 
     if email:
         token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": email, "reset_token": token}), 200
+        return jsonify({"email": email, "reset_token": token})
     else:
         abort(403)
 
