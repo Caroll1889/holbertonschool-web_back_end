00191230ffdd83edfdd88723dfae5ac35ea475e3
Caroll1889 <diahancaroll@hotmail.com>
2021-04-18 16:50:14 -0500

    Test utils methods

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Test for utils.py"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+from unittest.mock import patch
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class test for utils.access_nested_map"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Function that test utils.access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Class test for utils.get_json"""
