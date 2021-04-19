c6a438531cc182b2e8e512948ec1490515c14141
Caroll1889 <diahancaroll@hotmail.com>
2021-04-19 17:29:48 -0500

    Test for client file

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test for client.py"""
 
 import unittest
-from parameterized import parameterized, param
+from parameterized import parameterized
 from utils import requests, get_json
 from client import GitHubOrgClient
 from unittest.mock import Mock, patch
@@ -12,15 +12,15 @@ class TestGitHubOrgClient(unittest.TestCase):
     """Class test for client.py"""
 
     @parameterized.expand([
-        param(org="google", test_payload={"google": True}),
-        param(org="abc", test_payload={"abc": True})
+        ("google"),
+        ("abc")
     ])
+    @patch('client.get_json')
     def test_org(self, org, test_payload):
         """Method test that GitHubOrgClient.org
         returns the correct value.
         """
-        with patch('client.get_json', return_value=test_payload) as mock:
-            i = GitHubOrgClient(org)
-            res = i.org
-            self.assertEqual(res, test_payload)
-            mock.assert_called_once()
+        test_payload.return_value = True
+        i = GitHubOrgClient(org)
+        self.assertEqual(i.org, True)
+        test_payload.assert_called_once()
