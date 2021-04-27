5c13e307dcdfc36777ef0fa085fcdd88fe7bd7b8
Caroll1889 <diahancaroll@hotmail.com>
2021-04-27 16:31:14 -0500

    Add get_str and get_int methods

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -36,7 +36,7 @@ class Cache():
             else:
                 return result
         except Exception:
-            pass
+            return None
 
     def get_str(self, data: bytes) -> str:
         """Function that converts bytes in string"""
