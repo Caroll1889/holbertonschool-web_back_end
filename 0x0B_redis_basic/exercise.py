748f9931b0bc62e3bf552cc4d227a2a7e84a8274
Caroll1889 <diahancaroll@hotmail.com>
2021-04-28 12:24:47 -0500

    Add: parameter in replay method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -46,7 +46,7 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
-def replay(method: Callable):
+def replay(method: Callable) -> Callable:
     """
     Function that displays the history of calls of a particular function.
     """
