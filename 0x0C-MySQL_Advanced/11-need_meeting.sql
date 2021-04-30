a999c6aefa9feb935b0697d14a5bfeb81c1761cc
Caroll1889 <diahancaroll@hotmail.com>
2021-04-29 21:50:20 -0500

    No table for a meeting

---
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- Script that creates a view
+
+CREATE VIEW need_meeting AS
+SELECT name 
+FROM students
+WHERE score < 80
+AND last_meeting IS NULL
+OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH)
