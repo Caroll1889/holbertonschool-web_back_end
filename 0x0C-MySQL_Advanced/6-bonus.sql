88ef798f4c972d3e898eb150f54ebae372c09028
Caroll1889 <diahancaroll@hotmail.com>
2021-04-29 19:00:36 -0500

    Add bonus

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -7,12 +7,12 @@ CREATE PROCEDURE AddBonus (
         IN project_name VARCHAR(255),
         IN score INT)
 BEGIN
-    INSERT INTO projects(name)
+    INSERT INTO projects (name)
     SELECT project_name
     FROM DUAL
-    WHERE project_name NOT IN (SELECT name FROM projects)
+    WHERE project_name NOT IN (SELECT name FROM projects);
 
     INSERT INTO corrections (user_id, project_id, score)
-    VALUES(user_id(SELECT id FROM projects WHERE name = project_name), score);
-END $$
+    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+END;$$
 DELIMITER;
