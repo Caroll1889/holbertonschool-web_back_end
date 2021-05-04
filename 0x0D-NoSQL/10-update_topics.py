4b356369c05583708d87b5301f809cd9168abc9b
Caroll1889 <diahancaroll@hotmail.com>
2021-05-03 19:05:54 -0500

    Change school topics

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -3,11 +3,10 @@
 Change school topics
 """
 
+
 def update_topics(mongo_collection, name, topics):
-    """Function that changes all topics of a school document 
+    """Function that changes all topics of a school document
     based on the name
     """
-    return mongo_collection.update_many({'name': name}, 
+    return mongo_collection.update_many({'name': name},
                                         {'$set': {'topics': topics}})
-
-
