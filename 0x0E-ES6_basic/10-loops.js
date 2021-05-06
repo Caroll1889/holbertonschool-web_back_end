bf8a512117f9d8858f8e74166da2365dad846c30
Caroll1889 <diahancaroll@hotmail.com>
2021-05-06 08:25:00 -0500

    Fix: ESLint

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
-export default function appendToEachArrayValue (array, appendString) {
-  const newArray = []
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
   for (const element of array) {
-    newArray.push(`${appendString}${element}`)
+    newArray.push(`${appendString}${element}`);
   }
-  return newArray
+  return newArray;
 }
