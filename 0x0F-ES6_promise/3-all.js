e97731e78a6e56f3c7d8d91f9c60e01620cd3d5d
Caroll1889 <diahancaroll@hotmail.com>
2021-05-10 17:19:08 -0500

    multiple promises

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise = Promise.all([uploadPhoto(), createUser()])
+    .then((val) => {
+      console.log(`${val[0].body} ${val[1].firstName} ${val[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+  return promise;
+}
