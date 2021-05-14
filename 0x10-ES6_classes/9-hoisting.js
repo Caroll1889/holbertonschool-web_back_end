46750b62b7fd37805cf4a74e885c6fdf7f7f0b7a
Caroll1889 <diahancaroll@hotmail.com>
2021-05-14 15:03:52 -0500

    Add: fullStudentDescription method

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -33,11 +32,9 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - 
-            ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
