a708ac996bd26e425ba0609bc9602e47006c1380
Caroll1889 <diahancaroll@hotmail.com>
2021-05-18 11:20:21 -0500

    set method

---
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  const newArr = [...set];
+  if (!startString || startString.length === 0) {
+    return '';
+  }
+  const words = newArr.filter((x) => x.startsWith(startString)).map((x) => x.slice(3)).join('-');
+  return words;
+}
