ff02a85e867e38bbbefb97c68a3ee9a2809f24b5
Caroll1889 <diahancaroll@hotmail.com>
2021-05-18 11:26:40 -0500

    set method

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
   const newArr = [...set];
+  const newWord = startString.length;
   if (!startString || startString.length === 0) {
     return '';
   }
-  const words = newArr.filter((x) => x.startsWith(startString)).map((x) => x.slice(3)).join('-');
+  const words = newArr.filter((x) => x.startsWith(startString)).map((x) => x.slice(newWord)).join('-');
   return words;
 }
