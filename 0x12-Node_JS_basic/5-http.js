f402e6834992fcca8c62ac0ed85a5f1861874d53
Caroll1889 <diahancaroll@hotmail.com>
2021-05-22 14:32:18 -0500

    http server

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const hostname = '127.0.0.1'
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
@@ -14,11 +15,13 @@ const app = http.createServer(async (req, res) => {
     try {
       const data = await countStudents(process.argv[2]);
       res.end(`${data.join('\n')}`);
-    } catch (err) {
-      res.end(err.message);
+    } catch (error) {
+      res.end(error.message);
     }
   }
   res.end();
 });
-app.listen(port);
+app.listen(port, hostname, () => {
+  
+});
 module.exports = app;
