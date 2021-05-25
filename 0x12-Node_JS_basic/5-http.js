7d4153449dac1f6d51890b02b9da47f1306a5b21
Caroll1889 <diahancaroll@hotmail.com>
2021-05-25 14:51:57 -0500

    Fix:eslint

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -2,13 +2,15 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const port = 1245;
+const hostname = '127.0.0.1';
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
+  const { url } = req;
+  if (url === '/') {
     res.write('Hello Holberton School!');
-  } else if (req.url === '/students') {
+  } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
       const student = await countStudents(process.argv[2]);
@@ -20,5 +22,6 @@ const app = http.createServer(async (req, res) => {
   res.statusCode = 404;
   res.end();
 });
-app.listen(port);
+app.listen(port, hostname, () => {
+});
 module.exports = app;
